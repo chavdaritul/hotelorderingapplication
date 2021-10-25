ac74e373b03831463196d02e032a7c80121b8566
chavdaritul <92817185+chavdaritul@users.noreply.github.com>
2021-10-25 21:41:10 +0530

    login_implementation_main

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_mysqldb import MySQL
+from flask_bcrypt import Bcrypt
 import yaml
 
 app = Flask(__name__, static_url_path='')
+app.config['SECRET_KEY'] = '92b1e1bfefb207d960945555065173bf'
+bcrypt = Bcrypt()
 
 # Config DB
 db = yaml.load(open('db.yaml'))
@@ -15,20 +18,49 @@ mysql = MySQL(app)
 @app.route('/')
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    cur = mysql.connection.cursor()
     if request.method == 'POST':
         # Request Data from HTML file from
         userDetails = request.form
         userid = userDetails['userid']
         emailid = userDetails['emailid']
         password = userDetails['password']
+        confirmpassword = userDetails['confirmpassword']
 
-        # Create cursor to connect to database and execute CRUD (insert, update and delete) operations
-        cur = mysql.connection.cursor()
-        cur.execute('INSERT into customers(userid, emailid, password) values(%s, %s, %s)', (userid, emailid, password))
-        mysql.connection.commit()
-        cur.close()
-        return 'Success'
+        if password == confirmpassword:
+            # Create cursor to connect to database and execute CRUD (insert, update and delete) operations
+            password = bcrypt.generate_password_hash(password).decode('UTF-8')
+            
+            cur.execute('INSERT into customers(userid, emailid, password) values(%s, %s, %s)', (userid, emailid, password))
+            mysql.connection.commit()
+            cur.close()
+            return redirect(url_for('login'))
+        else:
+            print('Error')
     return render_template('register.html')
+            
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    cur = mysql.connection.cursor()
+    if request.method == 'POST':
+        userDetails = request.form
+        userid = userDetails['userid']
+        password = userDetails['password']
+        cur.execute("SELECT * FROM `customers` WHERE userid='%s'" % userid)
+        data = cur.fetchone()
+        cur.close()
+        if data:
+            print(bcrypt.check_password_hash(data[3], password))
+            if bcrypt.check_password_hash(data[3], password):
+                session['usedid'] = data[1]
+                return redirect(url_for('customer'))
+            else:
+                return '<b>Incorrect Password</b>'
+        elif data == None:
+            return '<b>No User ID Found!</b>'
+    else:
+        return render_template('login.html')
 
 @app.route('/customer')
 def customer():
